2cdb051ca51d9a0f074b43435e4be51265f3c4e1
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2020-09-25 18:40:14 +0530

    Nothing.

--- music_website/music/views.py
+++ music_website/music/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Genre
+from .models import Genre, Language
 from user.models import AdditionalArtistDetail
 
 def discover(request):
     genres = Genre.objects.all()
     artists = AdditionalArtistDetail.objects.all()
+    languages = Language.objects.all()
 
-    return render(request, 'music/discover.html', {'genres' : genres, 'artists' : artists})
+    return render(request, 'music/discover.html', {'genres' : genres, 'artists' : artists, 'languages' : languages})
